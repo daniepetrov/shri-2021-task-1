cf77e5f9e9b5e7a5dab185c872d56d56cdc85794
hookydev <dapetrov0@gmail.com>
2021-03-22 09:38:34 +0300

    Added activity isometric-line

--- src/js/templates/Activity.js
+++ src/js/templates/Activity.js
@@ -7,30 +7,58 @@ const Activity = (data) => {
         <div class="story__content content">
           <div class="story__heading">${data.title}</div>
           <h2 class="story__subheading">${data.subtitle}</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="73" viewBox="0 0 34 73">
-            <use xlink:href="#extra-dark"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="73" viewBox="0 0 34 73">
-            <use xlink:href="#extra-light"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="57" viewBox="0 0 34 57">
-            <use xlink:href="#max-dark"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="57" viewBox="0 0 34 57">
-            <use xlink:href="#max-light"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="41" viewBox="0 0 34 41">
-            <use xlink:href="#mid-dark"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="41" viewBox="0 0 34 41">
-            <use xlink:href="#mid-light"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
-            <use xlink:href="#min-dark"></use>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
-            <use xlink:href="#min-light"></use>
-          </svg>
+          <div class="isometric-line">
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="73" viewBox="0 0 34 73">
+              <use xlink:href="#extra-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="73" viewBox="0 0 34 73">
+              <use xlink:href="#extra-light"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="57" viewBox="0 0 34 57">
+              <use xlink:href="#max-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="57" viewBox="0 0 34 57">
+              <use xlink:href="#max-light"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="41" viewBox="0 0 34 41">
+              <use xlink:href="#mid-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="41" viewBox="0 0 34 41">
+              <use xlink:href="#mid-light"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
+              <use xlink:href="#min-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
+              <use xlink:href="#min-light"></use>
+            </svg>
+          </div>
+          <div class="isometric-line">
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="73" viewBox="0 0 34 73">
+              <use xlink:href="#extra-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="73" viewBox="0 0 34 73">
+              <use xlink:href="#extra-light"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="57" viewBox="0 0 34 57">
+              <use xlink:href="#max-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="57" viewBox="0 0 34 57">
+              <use xlink:href="#max-light"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="41" viewBox="0 0 34 41">
+              <use xlink:href="#mid-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="41" viewBox="0 0 34 41">
+              <use xlink:href="#mid-light"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
+              <use xlink:href="#min-dark"></use>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
+              <use xlink:href="#min-light"></use>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
