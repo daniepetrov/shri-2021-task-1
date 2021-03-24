ebb60bd40a32ec61d9d64255ebfae12e934ac81d
hookydev <dapetrov0@gmail.com>
2021-03-24 04:35:43 +0300

    Created Chart story

--- src/js/stories/Chart.js
+++ src/js/stories/Chart.js
@@ -1,13 +1,70 @@
 import { html } from '../utils'
 
+const CHART_OFFSET = 192
+const If = (condition, render) => (condition ? render : '')
+
+const getHeight = (value, data) => {
+  const maxHeightValue = data.values.find((item) => item.active).value
+  const height = ((value / maxHeightValue) * 100).toFixed()
+  return +height === 0 ? 'auto' : `${height}%`
+}
+
 const Chart = (data) => {
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      const itemAct = document.querySelector('.chart__item--active')
+      const chartInner = document.querySelector('.chart__inner')
+      const offsetLeft = itemAct.offsetLeft
+      chartInner.scrollLeft = offsetLeft - window.innerWidth + CHART_OFFSET
+    }, 400)
+  })
+
   return html`
-    <div class="story">
+    <div class="story chart">
       <div class="story__container">
-        <div class="story__content content">
+        <div class="story__content chart__content">
           <div class="story__heading">${data.title}</div>
-          <h2 class="story__subheading">${data.subtitle}</h2>
-          <div class="chart"></div>
+          <h2 class="story__caption">${data.subtitle}</h2>
+          <div class="chart__inner">
+            <div class="chart__grid">
+              ${data.values.mapj(
+                (item) =>
+                  html`
+                    <div
+                      class="chart__item ${If(item.active, 'chart__item--active')}"
+                      style="max-height: ${getHeight(item.value, data)}"
+                    >
+                      ${If(
+                        Boolean(item.value),
+                        html`<div class="chart__item-top">${item.value}</div>`,
+                      )}
+                      <div class="chart__item-bar">
+                        <div class="bar"></div>
+                      </div>
+                      <div class="chart__item-bottom">${item.title}</div>
+                    </div>
+                  `,
+              )}
+            </div>
+          </div>
+          <div class="chart__people">
+            ${data.users.mapj(
+              (user) =>
+                html`
+                  <li class="chart__people-column">
+                    <div class="card card--horizontal">
+                      <div class="card__avatar">
+                        <img src=${`/images/1x/${user.avatar}`} alt="" class="card__avatar-img" />
+                      </div>
+                      <div class="card__content">
+                        <div class="card__name">${user.name}</div>
+                        <div class="card__caption">${user.valueText}</div>
+                      </div>
+                    </div>
+                  </li>
+                `,
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/js/stories/Vote.js
+++ src/js/stories/Vote.js
@@ -13,39 +13,47 @@ const Vote = (data) => {
       <div class="story__container">
         <div class="story__content vote__content">
           <div class="story__heading">${data.title}</div>
-          <h2 class="story__caption">${data.subtitle}</h2>
-          <!-- <button class="arrow-button">
-            <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <use xlink:href="#arrow-button" />
-            </svg>
-          </button>
-          <button class="arrow-button arrow-button--reversed">
-            <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <use xlink:href="#arrow-button"></use>
-            </svg>
-          </button> -->
-          <ul class="vote__grid">
-            ${users.mapj(
-              (user, i) =>
-                html`
-                  <li class="vote__item ${i === 1 ? 'vote__item--active' : ''}">
-                    <div
-                      class="card ${If(selectedUser && i === selectedUserIndex, 'card--active')}"
+          <h2 class="story__caption vote__caption">${data.subtitle}</h2>
+          <div class="vote__content">
+            <ul class="vote__grid">
+              ${users.mapj(
+                (user, i) =>
+                  html`
+                    <li
+                      class="vote__item vote__item--${i + 1} ${If(i === 1, 'vote__item--active')}"
                     >
-                      ${selectedUser && i === selectedUserIndex
-                        ? html`<div class="card__emoji">👍</div>`
-                        : ''}
-                      <div class="card__avatar">
-                        <img src=${`/images/1x/${user.avatar}`} alt="" class="card__avatar-img" />
+                      <div
+                        class="card ${If(selectedUser && i === selectedUserIndex, 'card--active')}"
+                      >
+                        ${selectedUser && i === selectedUserIndex
+                          ? html`<div class="card__emoji">👍</div>`
+                          : ''}
+                        <div class="card__avatar">
+                          <img src=${`/images/1x/${user.avatar}`} alt="" class="card__avatar-img" />
+                        </div>
+                        <div class="card__content">
+                          <div class="card__name">${user.name}</div>
+                        </div>
                       </div>
-                      <div class="card__content">
-                        <div class="card__name">${user.name}</div>
-                      </div>
-                    </div>
-                  </li>
-                `,
-            )}
-          </ul>
+                    </li>
+                  `,
+              )}
+              <li class="vote__button vote__button--1">
+                <button class="arrow-button arrow-button--reversed" disabled>
+                  <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <use xlink:href="#arrow-button" />
+                  </svg>
+                </button>
+              </li>
+              <li class="vote__button vote__button--2">
+                <button class="arrow-button arrow-button">
+                  <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <use xlink:href="#arrow-button" />
+                  </svg>
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
